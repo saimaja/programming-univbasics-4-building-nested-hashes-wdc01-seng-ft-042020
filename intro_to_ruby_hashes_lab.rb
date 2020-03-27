9e3865d8038be021e65fac97c15bba8ad0f09e61
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-27 19:58:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monopoly_with_second_tier = {
-    :railroads => {pieces: 4, rent_in_dollars: {}, names: {}
+    :railroads => {:pieces=> 4, :rent_in_dollars => {}, :names => {}
   }
 end
 
