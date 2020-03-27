57d8f718d1277b41dd1b7067fc26e443484ef0db
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-27 19:59:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly_with_second_tier
   # complexity.
   base_hash = {
     :railroads => {
-      :pieces=> 4, 
+      :pieces => 4, 
       :rent_in_dollars => {}, 
       :names => {}
   }
